9495abfb2306ff41db363bbf46d2f8223905cb95
kljfy <314145293@qq.com>
2022-10-16 16:40:24 +0800

    init

---
+++ packages/components/src/button/button.vue
@@ -0,0 +1,10 @@
+<template>
+  <button class="btn">测试按钮</button>
+</template>
+<script lang="ts">
+import './button.less';
+import { defineComponent } from 'vue';
+export default defineComponent({
+  name: 'z-button',
+});
+</script>
